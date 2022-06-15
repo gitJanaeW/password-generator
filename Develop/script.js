2b77dd5780dfc0682e016d256c3feda8865fb67f
janae.wel <janae.wel@gmail.com>
2022-06-15 13:44:25 -0400

    code consolidated / 1 bug

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 // VARIABLES
+// id getters
 var password = document.getElementById("password");
 var lengthInput = document.getElementById("length-input");
 var upper = document.getElementById("upper");
@@ -9,155 +10,94 @@ var passwordLengthRangeError = document.getElementById("password-length-range-er
 var passwordLengthNaN = document.getElementById("password-length-NaN");
 var preferenceError = document.getElementById("preference-error");
 
+// Password preference vars
 var numbers = "1234567890";
 var lowLetters = "abcdefghijklmnopqrstuvwxyz";
 var upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialChars = "!@#$%^&*+-_'~.:;="
+var specialChars = "!@#$%^&*+-_'~.:;=";
 
+// Misc
 var passwordLength = "";
 var printOptions = "";
-
 var allPreferences = [
-  {type: "isUpper", boolValue: false},
-  {type: "isLower", boolValue: false},
-  {type: "isNum", boolValue: false},
-  {type: "isSpecial", boolValue: false}
+  upper, lower, nums, special
 ];
 
 function generatePassword(){
   function getLength(){
-    // reset error messages
+    // RESET ERROR MESSAGES
     passwordLengthRangeError.textContent = "";
     passwordLengthNaN.textContent = "";
     preferenceError.textContent = "";
 
     passwordLength = lengthInput.value;
 
-    // MAKE SURE LENGTH IS A NUMBER
-    console.log("MAKE SURE IT'S A NUMBER FUNCTION");    
+    // MAKE SURE LENGTH IS A NUMBER  
     // If passwordLength is NOT Not-a-Number (ie. if it IS a number)...
     if(!isNaN(passwordLength)){
-      console.log("Is a number");
       passwordLength = Math.round(parseInt(passwordLength));
-      console.log(passwordLength);
     }else{
       passwordLengthNaN.textContent = "Please enter a numeric value.";
-      console.log("Not a number");
       return;
     }
 
     // MAKE SURE THE LENGTH IS NOT TOO LONG/SHORT
-    console.log("MAKE SURE IT'S NOT TOO SHORT/LONG FUNCTION");
     if(passwordLength < 8 || passwordLength > 128 || !passwordLength){
-      passwordLengthRangeError.textContent = "Your password length must be between 5-15 characters.";
+      passwordLengthRangeError.textContent = "Your password length must be between 8-128 characters.";
       return;
     }
     else{
-      console.log("Within preference ranges.");
+      return passwordLength;
     }
-    return passwordLength;
+
   }
 
 
   function getCharPreference(){
-    allPreferences[0].boolValue = upper.checked;
-    allPreferences[1].boolValue = lower.checked;
-    allPreferences[2].boolValue = nums.checked;
-    allPreferences[3].boolValue = special.checked;
-    console.log("After prompts: ", allPreferences[0].boolValue, allPreferences[1].boolValue, allPreferences[2].boolValue, allPreferences[3].boolValue);
+    // STORE BOOL VALUE OF RADIO BUTTONS
+    for(var i = 0; i < allPreferences.length; i++){
+      allPreferences[i] = allPreferences[i].checked;
+    } 
+
+    // allPreferences[0] = upper.checked;
+    // allPreferences[1] = lower.checked;
+    // allPreferences[2] = nums.checked;
+    // allPreferences[3] = special.checked;
+    
+    console.log(upper.checked, lower.checked, nums.checked, special.checked);
 
     // CHECK THAT THERE ARE PREFERENCES
-    console.log("CHECK THAT THERE ARE PREFERENCES");
-    if(!allPreferences[0].boolValue && !allPreferences[1].boolValue && !allPreferences[2].boolValue && !allPreferences[3].boolValue){
+    if(!allPreferences[0] && !allPreferences[1] && !allPreferences[2] && !allPreferences[3]){
       preferenceError.textContent = "You must select at least 1 character type.";
       return;
     }
-    console.log("There are preferences.");
 
     // GATHER WANTED PREFERENCES
-    // Is this necessary?
-    console.log("GATHER WANTED PREFERENCES");
-    var wantedPreferences = [];
-
-
-    for(var i = 0; i < allPreferences.length; i++){
-      var x = 0;
-      if(allPreferences[i].boolValue){
-        console.log("wantedPreference: ", allPreferences[i].type, allPreferences[i].boolValue.toString());
-        wantedPreferences.push({type:allPreferences[i].type, boolValue:allPreferences[i].boolValue});
-        x++;
-      }
-    }
-
-    // STRING TOGETHER PREFERENCES
-    console.log("CREATE A STRING OF PREFERENCES");
-    var finalPreferences = "";
-
-    for(var i = 0; i < wantedPreferences.length; i++){
-      finalPreferences += wantedPreferences[i].type.toString();
-    }   
-    console.log(finalPreferences);
-
-    // MAKE A VAR OF PREFERRED CHARACTERS
-    console.log("MAKE A VAR OF PREFERRED CHARACTERS");
     var finalVar = "";
-
-    if(finalPreferences.includes("isUpper")){
+    if(allPreferences[0]){
       finalVar += upLetters;
-      console.log(finalVar);
     }
-    if(finalPreferences.includes("isLower")){
+    if(allPreferences[1]){
       finalVar += lowLetters;
-      console.log(finalVar);
     }
-    if(finalPreferences.includes("isNum")){
+    if(allPreferences[2]){
       finalVar += numbers;
-      console.log(finalVar);
     }
-    if(finalPreferences.includes("isSpecial")){
+    if(allPreferences[3]){
       finalVar += specialChars;
-      console.log(finalVar);
     }
-
-    console.log("finalVar", finalVar);
     return finalVar;
   }
 
   function printPassword(){
-    console.log("RANDOMIZE WANTED PREFERENCES INTO PASSWORD");
+    // RANDOMIZE WANTED PREFERENCES
     printOptions = getCharPreference();
     
     for(var i = 0; i < passwordLength; i++){
         password.innerHTML += printOptions.charAt(Math.floor(Math.random() * printOptions.length));
     }
-    console.log("password: ", password.innerHTML);
-
-
   }
 
   getLength();
   printPassword();
-
 }
-
-
-// START ALGORITHM
-// generatePassword();
-
-
-
-
-// // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword(); // Might need to change this to make it more specific
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
